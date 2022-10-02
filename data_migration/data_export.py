7a58a17b8c58882f2bf63011cb6cd0b4f049ee40
Gabriel Fauth <gabrielfauth@outlook.com>
2022-10-02 16:59:35 -0400

    adjusting export import data

---
+++ data_migration/data_export.py
@@ -0,0 +1,347 @@
+from operator import index
+import psycopg2 as pg
+import pandas as pd
+
+class data_export():
+    def con(self):
+        con = pg.connect(host='localhost', database='helpdesk',
+        user='postgres', password='postgres')
+        return con
+
+    def company(self):
+        conn = self.con()
+        cur = conn.cursor()
+        sql = '''
+        select
+            *
+        from default_company f
+        '''
+        cur.execute(sql)
+
+        data = []
+        for c in cur.fetchall():
+            data.append({
+                'id': c[0],
+                'slug': c[1],
+                'company': c[2],
+                'cnpj': c[3],
+                'created': str(c[4]),
+                'updated': str(c[5]),
+                'owner_id': c[6],
+            })
+        table = pd.DataFrame(data).to_excel("exports/company.xlsx", index=False)
+        conn.close()
+        
+    def companyposition(self):
+        conn = self.con()
+        cur = conn.cursor()
+        sql = '''
+        select
+            *
+        from default_companyposition f
+        '''
+        cur.execute(sql)
+
+        data = []
+        for c in cur.fetchall():
+            data.append({
+                'id': c[0],
+                'position': c[1],
+                'created': str(c[2]),
+                'updated': str(c[3]),
+                'company_id': c[4],
+            })
+        table = pd.DataFrame(data).to_excel("exports/companyposition.xlsx", index=False)
+        conn.close()
+    
+    def user(self):
+        conn = self.con()
+        cur = conn.cursor()
+        sql = '''
+        select
+            *
+        from auth_user f
+        '''
+        cur.execute(sql)
+
+        data = []
+        for c in cur.fetchall():
+            data.append({
+                'id': c[0],
+                'password': c[1],
+                'last_login': str(c[2]),
+                'is_superuser': c[3],
+                'username': c[4],
+                'first_name': c[5],
+                'last_name': c[6],
+                'email': c[7],
+                'is_staff': c[8],
+                'is_active': c[9],
+                'date_joined': str(c[10]),
+            })
+        table = pd.DataFrame(data).to_excel("exports/user.xlsx", index=False)
+        conn.close()
+
+    def companyworker(self):
+        conn = self.con()
+        cur = conn.cursor()
+        sql = '''
+        select
+            *
+        from default_companyworker f
+        '''
+        cur.execute(sql)
+
+        data = []
+        for c in cur.fetchall():
+            data.append({
+                'id': c[0],
+                'cpf': c[1],
+                'rg': c[2],
+                'phone_number': c[3],
+                'created': str(c[4]),
+                'updated': str(c[5]),
+                'company_id': c[6],
+                'person_id': c[7],
+                'position_id': c[8],
+            })
+        table = pd.DataFrame(data).to_excel("exports/companyworker.xlsx", index=False)
+        conn.close()
+        
+    def bugreport(self):
+        conn = self.con()
+        cur = conn.cursor()
+        sql = '''
+        select
+            *
+        from default_bugreport f
+        '''
+        cur.execute(sql)
+
+        data = []
+        for c in cur.fetchall():
+            data.append({
+                'id': c[0],
+                'bug': c[1],
+                'created': str(c[2]),
+                'updated': str(c[3]),
+                'company_id': c[4],
+                'company_worker_id': c[5],
+            })
+        table = pd.DataFrame(data).to_excel("exports/bugreport.xlsx", index=False)
+        conn.close()
+
+    def newregister(self):
+        conn = self.con()
+        cur = conn.cursor()
+        sql = '''
+        select
+            *
+        from default_newregister f
+        '''
+        cur.execute(sql)
+
+        data = []
+        for c in cur.fetchall():
+            data.append({
+                'id': c[0],
+                'email': c[1],
+                'first_name': c[2],
+                'last_name': c[3],  
+                'phone_number': c[4],
+                'created': str(c[5]),
+                'updated': str(c[6]),
+            })
+        table = pd.DataFrame(data).to_excel("exports/newregister.xlsx", index=False)
+        conn.close()
+
+    def productbrand(self):
+        conn = self.con()
+        cur = conn.cursor()
+        sql = '''
+        select
+            *
+        from foodservice_productbrand f
+        '''
+        cur.execute(sql)
+
+        data = []
+        for c in cur.fetchall():
+            data.append({
+                'id': c[0],
+                'brand': c[1],
+                'created': str(c[2]),
+                'updated': str(c[3]),
+                'company_id': c[4],
+                'company_worker_id': c[5],
+            })
+        table = pd.DataFrame(data).to_excel("exports/productbrand.xlsx", index=False)
+        conn.close()
+
+    def productmeasure(self):
+        conn = self.con()
+        cur = conn.cursor()
+        sql = '''
+        select
+            *
+        from foodservice_productmeasure f
+        '''
+        cur.execute(sql)
+
+        data = []
+        for c in cur.fetchall():
+            data.append({
+                'id': c[0],
+                'measure': c[1],
+                'created': str(c[2]),
+                'updated': str(c[3]),
+                'company_id': c[4],
+                'company_worker_id': c[5],
+            })
+        table = pd.DataFrame(data).to_excel("exports/productmeasure.xlsx", index=False) 
+        conn.close()
+
+    # def productgroup(self):
+    #     conn = self.con()
+    #     cur = conn.cursor()
+    #     sql = '''
+    #     select
+    #         *
+    #     from foodservice_groups f
+    #     '''
+    #     cur.execute(sql)
+
+    #     data = []
+    #     for c in cur.fetchall():
+    #         data.append({
+    #             'id': c[0],
+    #             'name': c[1],
+    #             'created': str(c[2]),
+    #             'updated': str(c[3]),
+
+    #         })
+    #     table = pd.DataFrame(data).to_excel("exports/groups.xlsx", index=False)
+    #     conn.close()
+
+    def product(self):
+        conn = self.con()
+        cur = conn.cursor()
+        sql = '''
+        select
+            *
+        from foodservice_product f
+        '''
+        cur.execute(sql)
+
+        data = []
+        for c in cur.fetchall():
+            data.append({
+                'id': c[0],
+                'name': c[1],
+                'stock': c[2],
+                'cost': c[3],
+                'created': str(c[4]),
+                'updated': str(c[5]),
+                'brand_id': c[6],
+                'company_id': c[7],
+                'company_worker_id': c[8],
+                'measure_id': c[9],
+                'price': c[10],
+                'type': c[11],
+            })
+        table = pd.DataFrame(data).to_excel("exports/product.xlsx", index=False)
+        conn.close()
+
+    def productitems(self):
+        conn = self.con()
+        cur = conn.cursor()
+        sql = '''
+        select
+            *
+        from foodservice_productitems f
+        '''
+        cur.execute(sql)
+
+        data = []
+        for c in cur.fetchall():
+            data.append({
+                'id': c[0],
+                'created': str(c[1]),
+                'updated': str(c[2]),
+                'company_id': c[3],
+                'company_worker_id': c[4],
+                'product_id': c[5],
+                'product_item_id': c[6],
+                'quantity': c[7],
+
+            })
+        table = pd.DataFrame(data).to_excel("exports/productitems.xlsx", index=False)
+        conn.close()
+
+    def sale(self):
+        conn = self.con()
+        cur = conn.cursor()
+        sql = '''
+        select
+            *
+        from foodservice_sale f
+        '''
+        cur.execute(sql)
+
+        data = []
+        for c in cur.fetchall():
+            data.append({
+                'id': c[0],
+                'created': str(c[1]),
+                'updated': str(c[2]),
+                'company_id': c[3],
+                'company_worker_id': c[4],
+                'delivery': c[5],
+                'value': c[6],
+                'total': c[7],
+
+            })
+        table = pd.DataFrame(data).to_excel("exports/sale.xlsx", index=False)
+        conn.close()
+
+    def saleitems(self):
+        conn = self.con()
+        cur = conn.cursor()
+        sql = '''
+        select
+            *
+        from foodservice_saleitems f
+        '''
+        cur.execute(sql)
+
+        data = []
+        for c in cur.fetchall():
+            data.append({
+                'id': c[0],
+                'company_id': c[3],
+                'company_worker_id': c[4],
+                'product_id': c[5],
+                'price': c[6],
+                'quantity': c[7],
+                'sale_id': c[8],
+                'created': str(c[1]),
+                'updated': str(c[2]),
+
+            })
+        table = pd.DataFrame(data).to_excel("exports/saleitems.xlsx", index=False)
+        conn.close()
+
+if __name__ == "__main__":
+    data_export().company()
+    data_export().companyposition()
+    data_export().user()
+    data_export().companyworker()
+    data_export().bugreport()
+    data_export().newregister()
+    data_export().productbrand()
+    # data_export().groups()
+    data_export().productmeasure()
+    data_export().product()
+    data_export().productitems()
+    data_export().sale()
+    data_export().saleitems()
---
+++ data_migration/data_import.py
@@ -0,0 +1,537 @@
+import pandas as pd
+import psycopg2 as pg
+
+class data_import():
+    def con(self):
+        con = pg.connect(host='localhost', database='foodservice',
+        user='postgres', password='postgres')
+        return con
+
+    def company(self):
+        table = pd.read_excel("exports/company.xlsx")
+        i = 0
+        try:
+            len_id = len(table['id'])
+        except:
+            len_id = 0
+        while i < len_id:
+            try:
+                conn = self.con()
+                cur = conn.cursor()
+                cur.execute(f'''
+                insert into default_company (
+                    id, 
+                    slug, 
+                    company, 
+                    cnpj, 
+                    owner_id, 
+                    created, 
+                    updated
+                )
+                values (
+                    {table['id'][i]},
+                    '{table['slug'][i]}',
+                    '{table['company'][i]}',
+                    '{table['cnpj'][i]}',
+                    {table['owner_id'][i]},
+                    '{table['created'][i]}',
+                    '{table['updated'][i]}'
+                ) 
+                ''')
+                conn.commit()
+                conn.close()
+            except:
+                conn = self.con()
+                cur = conn.cursor()
+                cur.execute(f'''
+                update default_company set
+                    slug = '{table['slug'][i]}',
+                    company = '{table['company'][i]}',
+                    cnpj = '{table['cnpj'][i]}',
+                    owner_id = {table['owner_id'][i]},
+                    created = '{table['created'][i]}',
+                    updated = '{table['updated'][i]}'
+                where
+                    id = {table['id'][i]}
+                ''')
+                conn.commit()
+                conn.close()
+            i+=1
+
+    def companyposition(self):
+        table = pd.read_excel("exports/companyposition.xlsx")
+        i = 0
+        try:
+            len_id = len(table['id'])
+        except:
+            len_id = 0
+        while i < len_id:
+            try:
+                conn = self.con()
+                cur = conn.cursor()
+                cur.execute(f'''
+                insert into default_companyposition (
+                    id, 
+                    position,
+                    company_id, 
+                    created, 
+                    updated
+                )
+                values (
+                    {table['id'][i]},
+                    '{table['position'][i]}',
+                    {table['company_id'][i]},
+                    '{table['created'][i]}',
+                    '{table['updated'][i]}'
+                )
+                ''')
+                conn.commit()
+                conn.close()
+            except:
+                conn = self.con()
+                cur = conn.cursor()
+                cur.execute(f'''
+                update default_companyposition set
+                    position = '{table['position'][i]}',
+                    company_id = {table['company_id'][i]},
+                    created = '{table['created'][i]}',
+                    updated = '{table['updated'][i]}'
+                where
+                    id = {table['id'][i]}
+                ''')
+                conn.commit()
+                conn.close()
+            i+=1
+
+    def companyworker(self):
+        table = pd.read_excel("exports/companyworker.xlsx")
+        i = 0
+        try:
+            len_id = len(table['id'])
+        except:
+            len_id = 0
+        while i < len_id:
+            try:
+                conn = self.con()
+                cur = conn.cursor()
+                cur.execute(f'''
+                insert into default_companyworker (
+                    id,
+                    cpf,
+                    rg,
+                    phone_number,
+                    company_id,
+                    person_id,
+                    position_id,
+                    created,
+                    updated
+                )
+                values (
+                    {table['id'][i]},
+                    '{table['cpf'][i]}',
+                    '{table['rg'][i]}',
+                    '{table['phone_number'][i]}',
+                    {table['company_id'][i]},
+                    {table['person_id'][i]},
+                    {table['position_id'][i]},
+                    '{table['created'][i]}',
+                    '{table['updated'][i]}'
+                )
+                ''')
+                conn.commit()
+                conn.close()
+            except:
+                conn = self.con()
+                cur = conn.cursor()
+                cur.execute(f'''
+                update default_companyworker set
+                    cpf = '{table['cpf'][i]}',
+                    rg = '{table['rg'][i]}',
+                    phone_number = '{table['phone_number'][i]}',
+                    company_id = {table['company_id'][i]},
+                    person_id = {table['person_id'][i]},
+                    position_id = {table['position_id'][i]},
+                    created = '{table['created'][i]}',
+                    updated = '{table['updated'][i]}'
+                where
+                    id = {table['id'][i]}
+                ''')
+                conn.commit()
+                conn.close()
+            i+=1
+
+    def bugreport(self):
+        table = pd.read_excel("exports/bugreport.xlsx")
+        i = 0
+        try:
+            len_id = len(table['id'])
+        except:
+            len_id = 0
+        while i < len_id:
+            conn = self.con()
+            cur = conn.cursor()
+            cur.execute(f'''
+            insert into default_bugreport (
+                id,
+                bug,
+                company_id,
+                company_worker_id,
+                created,
+                updated
+            )
+            values (
+                {table['id'][i]},
+                {table['bug'][i]},
+                {table['company_id'][i]},
+                {table['company_worker_id'][i]},
+                '{table['created'][i]}',
+                '{table['updated'][i]}'
+            )
+            ''')
+            conn.commit()
+            conn.close()
+            i+=1
+
+    def newregister(self):
+        table = pd.read_excel("exports/newregister.xlsx")
+        i = 0
+        try:
+            len_id = len(table['id'])
+        except:
+            len_id = 0
+        while i < len_id:
+            try: 
+                conn = self.con()
+                cur = conn.cursor()
+                cur.execute(f'''
+                insert into default_newregister (
+                    id,
+                    email,
+                    first_name,
+                    last_name,
+                    phone_number,
+                    created,
+                    updated
+                )
+                values (
+                    {table['id'][i]},
+                    '{table['email'][i]}',
+                    "{table['first_name'][i]}",
+                    "{table['last_name'][i]}",
+                    '{table['phone_number'][i]}',
+                    '{table['created'][i]}',
+                    '{table['updated'][i]}'
+                )
+                ''')
+                conn.commit()
+                conn.close()
+            except: 
+                conn = self.con()
+                cur = conn.cursor()
+                cur.execute(f'''
+                update default_newregister set
+                    email = '{table['email'][i]}',
+                    first_name = '{table['first_name'][i].replace("'", '"')}',
+                    last_name = '{table['last_name'][i].replace("'", '"')}',
+                    phone_number = '{table['phone_number'][i]}',
+                    created = '{table['created'][i]}',
+                    updated = '{table['updated'][i]}'
+                where
+                    id = {table['id'][i]}
+                ''')
+                conn.commit()
+                conn.close()
+            i+=1
+
+    def productbrand(self):
+        table = pd.read_excel("exports/productbrand.xlsx")
+        i = 0
+        try:
+            len_id = len(table['id'])
+        except:
+            len_id = 0
+        while i < len_id:
+            try:
+                conn = self.con()
+                cur = conn.cursor()
+                cur.execute(f'''
+                insert into register_productbrand (
+                    id,
+                    brand,
+                    company_id,
+                    company_worker_id,
+                    created,
+                    updated
+                )
+                values (
+                    {table['id'][i]},
+                    {table['brand'][i]},
+                    {table['company_id'][i]},
+                    {table['company_worker_id'][i]},
+                    '{table['created'][i]}',
+                    '{table['updated'][i]}'
+                )
+                ''')
+                conn.commit()
+                conn.close()
+            except:
+                conn = self.con()
+                cur = conn.cursor()
+                cur.execute(f'''
+                update register_productbrand set
+                    brand = '{table['brand'][i]}',
+                    company_id = {table['company_id'][i]},
+                    company_worker_id = {table['company_worker_id'][i]},
+                    created = '{table['created'][i]}',
+                    updated = '{table['updated'][i]}'
+                where 
+                    id = {table['id'][i]}
+                ''')
+                conn.commit()
+                conn.close()
+            i+=1
+
+    def productmeasure(self):
+        table = pd.read_excel("exports/productmeasure.xlsx")
+        i = 0
+        try:
+            len_id = len(table['id'])
+        except:
+            len_id = 0
+        while i < len_id:
+            try:
+                conn = self.con()
+                cur = conn.cursor()
+                cur.execute(f'''
+                insert into register_productmeasure (
+                    id,
+                    measure,
+                    company_id,
+                    company_worker_id,
+                    created,
+                    updated
+                )
+                values (
+                    {table['id'][i]},
+                    {table['measure'][i]},
+                    {table['company_id'][i]},
+                    {table['company_worker_id'][i]},
+                    '{table['created'][i]}',
+                    '{table['updated'][i]}'
+                )
+                ''')
+                conn.commit()
+                conn.close()
+            except:
+                conn = self.con()
+                cur = conn.cursor()
+                cur.execute(f'''
+                update register_productmeasure set
+                    measure = '{table['measure'][i]}',
+                    company_id = {table['company_id'][i]},
+                    company_worker_id = {table['company_worker_id'][i]},
+                    created = '{table['created'][i]}',
+                    updated = '{table['updated'][i]}'
+                where
+                    id = {table['id'][i]}
+                ''')
+                conn.commit()
+                conn.close()
+            i+=1
+
+    def product(self):
+        table = pd.read_excel("exports/product.xlsx")
+        i = 0
+        try:
+            len_id = len(table['id'])
+        except:
+            len_id = 0
+        while i < len_id:
+            try:
+                conn = self.con()
+                cur = conn.cursor()
+                cur.execute(f'''
+                insert into register_product (
+                    id,
+                    name,
+                    stock,
+                    cost,
+                    brand_id,
+                    measure_id,
+                    price,
+                    type,
+                    company_id,
+                    company_worker_id,
+                    created,
+                    updated
+                )
+                values (
+                    {table['id'][i]},
+                    '{table['name'][i]}',
+                    {table['stock'][i]},
+                    {table['cost'][i]},
+                    {table['brand_id'][i]},
+                    {table['measure_id'][i]},
+                    {table['price'][i]},
+                    {table['type'][i]},
+                    {table['company_id'][i]},
+                    {table['company_worker_id'][i]},
+                    '{table['created'][i]}',
+                    '{table['updated'][i]}'
+                )
+                ''')
+                conn.commit()
+                conn.close()
+            except:
+                conn = self.con()
+                cur = conn.cursor()
+                cur.execute(f'''
+                update register_product set
+                    {table['id'][i]},
+                    '{table['name'][i]}',
+                    {table['stock'][i]},
+                    {table['cost'][i]},
+                    {table['brand_id'][i]},
+                    {table['measure_id'][i]},
+                    {table['price'][i]},
+                    {table['type'][i]},
+                    {table['company_id'][i]},
+                    {table['company_worker_id'][i]},
+                    '{table['created'][i]}',
+                    '{table['updated'][i]}'
+                )
+                ''')
+                conn.commit()
+                conn.close()
+            i+=1
+
+    def productitems(self):
+        table = pd.read_excel("exports/productitems.xlsx")
+        i = 0
+        try:
+            len_id = len(table['id'])
+        except:
+            len_id = 0
+        while i < len_id:
+            try:
+                conn = self.con()
+                cur = conn.cursor()
+                cur.execute(f'''
+                insert into register_productitems (
+                    id,
+                    product_id,
+                    productitem_id,
+                    quantity,
+                    company_id,
+                    company_worker_id,
+                    created,
+                    updated
+                )
+                values (
+                    {table['id'][i]},
+                    {table['product_id'][i]},
+                    {table['productitem_id'][i]},
+                    {table['quantity'][i]},
+                    {table['company_id'][i]},
+                    {table['company_worker_id'][i]},
+                    '{table['created'][i]}',
+                    '{table['updated'][i]}'
+                )
+                ''')
+                conn.commit()
+                conn.close()
+            except:
+                conn = self.con()
+                cur = conn.cursor()
+                cur.execute(f'''
+                update register_productitems set
+                    product_id = {table['product_id'][i]},
+                    productitem_id = {table['productitem_id'][i]},
+                    quantity = {table['quantity'][i]},
+                    company_id = {table['company_id'][i]},
+                    company_worker_id = {table['company_worker_id'][i]},
+                    created = '{table['created'][i]}',
+                    updated = '{table['updated'][i]}'
+                where
+                    id = {table['id'][i]}
+                ''')
+                conn.commit()
+                conn.close()
+
+    def sale(self):
+        table = pd.read_excel("exports/sale.xlsx")
+        i = 0
+        while i < len(table['id']):
+            conn = self.con()
+            cur = conn.cursor()
+            cur.execute(f'''
+            insert into register_saleitems (
+                id,
+                delivery,
+                value,
+                total,
+                company_id,
+                company_worker_id,
+                created,
+                updated
+            )
+            values (
+                {table['id'][i]},
+                {table['delivery'][i]},
+                {table['value'][i]},
+                {table['total'][i]},
+                {table['company_id'][i]},
+                {table['company_worker_id'][i]},
+                '{table['created'][i]}',
+                '{table['updated'][i]}'
+            )
+            ''')
+            conn.commit()
+            conn.close()
+            i+=1
+
+    def saleitems(self):
+        table = pd.read_excel("exports/saleitems.xlsx")
+        i = 0
+        while i < len(table['id']):
+            conn = self.con()
+            cur = conn.cursor()
+            cur.execute(f'''
+            insert into register_saleitems (
+                id,
+                sale_id,
+                product_id,
+                quantity,
+                price,
+                company_id,
+                company_worker_id,
+                created,
+                updated
+            )
+            values (
+                {table['id'][i]},
+                {table['sale_id'][i]},
+                {table['product_id'][i]},
+                {table['quantity'][i]},
+                {table['price'][i]},
+                {table['company_id'][i]},
+                {table['company_worker_id'][i]},
+                '{table['created'][i]}',
+                '{table['updated'][i]}'
+            )
+            ''')
+            conn.commit()
+            conn.close()
+            i+=1
+
+
+
+
+if __name__ == '__main__':
+    data_import().company()
+    data_import().companyposition()
+    data_import().companyworker()
+    data_import().bugreport()
+    data_import().newregister()
+    data_import().productbrand()
+    data_import().productmeasure()
