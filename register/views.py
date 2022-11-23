ef71823b0fe3e7b257c6a7aea36fc9db8fa1528e
Gabriel Fauth <gabrielfauth@outlook.com>
2022-11-22 23:02:23 -0400

    if else for nonetipe brand and measure

--- register/views.py
+++ register/views.py
@@ -98,7 +98,7 @@ def getProduct(request):
                     'name': str(m.name),
                     'brand_id': str(m.brand.id) if type(m.brand) != type(None) else '',
                     'brand': str(m.brand),
-                    'measure_id': str(m.measure.id) if str(m.measure) != type(None) else '',
+                    'measure_id': str(m.measure.id) if type(m.measure) != type(None) else '',
                     'measure': str(m.measure),
                     'stock': str(m.stock),
                     'cost': str(round(m.cost,2)) if type(m.cost) != type(None) else '0',
